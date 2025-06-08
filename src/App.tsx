af8309c3bd699c566f4e6c5ae57388b5f3899f0a
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-06-08 20:55:19 +0530

    Landing page was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {LandingPage} from "./view/pages/LandingPage/LandingPage";
+
+
+function AppContent() {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage/>}/>
+        </Routes>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-        <p className="text-red-600 bg-blue-950">
-          Hello world!
-        </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <AppContent/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
