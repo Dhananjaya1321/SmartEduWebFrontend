9449d1f1879fbf14137c581a5f4b7e143caa8fdc
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-06-11 16:03:19 +0530

    Updated

--- src/view/component/DashboardTopNavBar/DashboardTopNavBar.tsx
+++ src/view/component/DashboardTopNavBar/DashboardTopNavBar.tsx
@@ -16,7 +16,7 @@ export const DashboardTopNavBar = () => {
 */
     return (
         <nav className='bg-white w-screen h-[75px] top-0 left-0 right-[20px] fixed shadow z-40
-        flex flex-row justify-between gap-1 items-center'>
+        flex flex-row justify-end gap-1 items-center'>
             <div className="flex flex-row items-center justify-center gap-2 mr-8">
                 <button
                     className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
