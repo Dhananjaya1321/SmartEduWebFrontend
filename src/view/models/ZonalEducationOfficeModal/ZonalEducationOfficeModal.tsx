ebcbf43a6f90e85de1fac0bf2abe232ba4d3dfc8
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-06-13 16:37:04 +0530

    ZonalEducationOfficeModal was created

--- src/view/models/ZonalEducationOfficeModal/ZonalEducationOfficeModal.tsx
+++ src/view/models/ZonalEducationOfficeModal/ZonalEducationOfficeModal.tsx
@@ -8,7 +8,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPen, faTimes} from "@fortawesome/free-solid-svg-icons";
 import {TextField} from "../../component/TextField/TextField";
 import {DropdownField} from "../../component/DropdownField/DropdownField";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -24,23 +24,81 @@ const style = {
     p: 4,
     overflowY: 'auto' // Enable scrolling if content overflows
 };
-const provinceDistrictMap: Record<string, string> = {
-    "Western": "Western",
-    "Central": "Central",
-    "Southern": "Southern",
-    "Northern": "Northern",
-    "Eastern": "Eastern",
-    "North Western": "North Western",
-    "North Central": "North Central",
-    "Uva": "Uva",
-    "Sabaragamuwa": "Sabaragamuwa"
+
+const provinceDistrictMap: Record<string, string[]> = {
+    "Western": ["Colombo", "Gampaha", "Kalutara"],
+    "Central": ["Kandy", "Matale", "Nuwara Eliya"],
+    "Southern": ["Galle", "Matara", "Hambantota"],
+    "Northern": ["Jaffna", "Mannar", "Vavuniya", "Mullaitivu", "Kilinochchi"],
+    "Eastern": ["Batticaloa", "Ampara", "Trincomalee"],
+    "North Western": ["Kurunegala", "Puttalam"],
+    "North Central": ["Anuradhapura", "Polonnaruwa"],
+    "Uva": ["Badulla", "Moneragala"],
+    "Sabaragamuwa": ["Ratnapura", "Kegalle"]
 };
-export default function ProvincialEducationOfficeModal() {
+export const districtZoneMap: Record<string, string[]> = {
+    "Colombo": ["Colombo", "Homagama", "Piliyandala", "Sri Jayawardanapura"],
+    "Gampaha": ["Gampaha", "Kelaniya", "Minuwangoda", "Negombo"],
+    "Kalutara": ["Horana", "Kalutara", "Matugama"],
+
+    "Kandy": ["Denuwara", "Gampola", "Kandy", "Katugastota", "Teldeniya", "Wathegama"],
+    "Matale": ["Galewela", "Matale", "Naula", "Wilgamuwa"],
+    "Nuwara Eliya": ["Hanguranketha", "Hatton", "Kotmale", "Nuwara Eliya", "Walapane"],
+
+    "Galle": ["Ambalangoda", "Elpitiya", "Galle", "Udugama"],
+    "Matara": ["Akuressa", "Matara", "Morawaka", "Mulatiyana (Hakmana)"],
+    "Hambantota": ["Hambantota", "Tangalle", "Walasmulla"],
+
+    "Jaffna": ["Islands", "Jaffna", "Thenmarachchi", "Vadamarachchi", "Valikamam"],
+    "Mannar": ["Madhu", "Mannar"],
+    "Vavuniya": ["Vavuniya North", "Vavuniya South"],
+    "Mullaitivu": ["Mullaitivu", "Thunukkai"],
+    "Kilinochchi": ["Kilinochchi North", "Kilinochchi South"],
+
+    "Batticaloa": ["Batticaloa", "Batticaloa Central", "Batticaloa West", "Kalkudah", "Paddiruppu"],
+    "Ampara": ["Akkaraipattu", "Ampara", "Dehiattakandiya", "Kalmunai", "Mahaoya", "Sammanthurai", "Thirukkovil"],
+    "Trincomalee": ["Kantale", "Kinniya", "Muttur", "Trincomalee", "Trincomalee North"],
+
+    "Kurunegala": ["Giriulla", "Ibbagamuwa", "Kuliyapitiya", "Kurunegala", "Maho", "Nikaweratiya"],
+    "Puttalam": ["Chilaw", "Puttalam"],
+
+    "Anuradhapura": ["Anuradhapura", "Galenbindunuwewa", "Kebithigollewa", "Kekirawa", "Thambuttegama"],
+    "Polonnaruwa": ["Dimbulagala", "Hingurakgoda", "Polonnaruwa"],
+
+    "Badulla": ["Badulla", "Bandarawela", "Mahiyanganaya", "Passara", "Viyaluwa", "Welimada"],
+    "Moneragala": ["Bibile", "Moneragala", "Thanamalwila", "Wellawaya"],
+
+    "Ratnapura": ["Balangoda", "Embilipitiya", "Nivitigala", "Ratnapura"],
+    "Kegalle": ["Dehiowita", "Kegalle", "Mawanella"]
+};
+
+export default function ZonalEducationOfficeModal() {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [selectedProvince, setSelectedProvince] = useState('');
+    const [districts, setDistricts] = useState<string[]>([]);
+    const [selectedDistrict, setSelectedDistrict] = useState('');
+    const [zonals, setZonals] = useState<string[]>([]);
+    const [selectedZonal, setSelectedZonal] = useState('');
 
+    // Load districts when province comes from DB
+    useEffect(() => {
+        if ("Western") {
+            const loadedDistricts = provinceDistrictMap["Western"] || [];
+            setDistricts(loadedDistricts);
+            setSelectedDistrict('');
+            setZonals([]);
+            setSelectedZonal('');
+        }
+    }, ["Western"]);
+
+    // Load zonals when a district is selected
+    const handleDistrictChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const district = e.target.value;
+        setSelectedDistrict(district);
+        setZonals(districtZoneMap[district] || []);
+        setSelectedZonal('');
+    };
     return (
         <div>
             <button
@@ -59,40 +117,48 @@ export default function ProvincialEducationOfficeModal() {
                         <FontAwesomeIcon icon={faTimes} size="lg"/>
                     </button>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Create Provincial Education Office
+                        Create Zonal Education Office
                     </Typography>
                     <section
                         className='bg-white flex flex-row flex-wrap items-center justify-center mt-5 p-5 rounded-xl shadow-md'>
                         <section
                             className='w-full flex flex-row flex-wrap items-center justify-center'>
                             <div className="w-full items-start flex my-2">
-                                <h3 className="font-medium">Provincial Education Office Details</h3>
+                                <h3 className="font-medium">Zonal Education Office Details</h3>
                             </div>
                             <div className='flex flex-row flex-wrap items-center justify-center w-full'>
                                 <DropdownField
-                                    important={"*"}
+                                    label="Select District"
+                                    important="*"
                                     mt={"12px"}
                                     mb={"12px"}
                                     ml={"12px"}
                                     mr={"12px"}
-                                    label={"Select province"}
-                                    options={
-                                        [
-                                            {label: 'Select...', value: ''},
-                                            ...Object.keys(provinceDistrictMap).map(p => ({label: p, value: p}))
-                                        ]
-                                    }
-                                    value={selectedProvince}
-                                    onChange={(e) => {
-                                        const province = e.target.value;
-                                        setSelectedProvince(province);
-                                    }}
+                                    options={[{label: 'Select...', value: ''}, ...districts.map(d => ({
+                                        label: d,
+                                        value: d
+                                    }))]}
+                                    value={selectedDistrict}
+                                    onChange={handleDistrictChange}
                                 />
-
+                                <DropdownField
+                                    label="Select Zonal"
+                                    important="*"
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={[{label: 'Select...', value: ''}, ...zonals.map(z => ({
+                                        label: z,
+                                        value: z
+                                    }))]}
+                                    value={selectedZonal}
+                                    onChange={(e) => setSelectedZonal(e.target.value)}
+                                />
                             </div>
                             <div className='flex flex-row flex-wrap items-center justify-center w-full'>
                                 <TextArea
-                                    name="provincialAddress"
+                                    name="zonalAddress"
                                     placeholder={'ex- ABC Road, Galle'}
                                     label={'Address'}
 
