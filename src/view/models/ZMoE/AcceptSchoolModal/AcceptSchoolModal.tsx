1a967386c6a04b014dedc2e2ccba160f48806a27
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-06-15 20:32:11 +0530

    AcceptSchoolModal was created

---
+++ src/view/models/ZMoE/AcceptSchoolModal/AcceptSchoolModal.tsx
@@ -0,0 +1,366 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {TextArea} from "../../../component/TextArea/TextArea";
+import {Button} from "../../../component/Button/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faTimes} from "@fortawesome/free-solid-svg-icons";
+import {TextField} from "../../../component/TextField/TextField";
+import {DropdownField} from "../../../component/DropdownField/DropdownField";
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70%',
+    maxHeight: '90vh', // Ensure the modal doesn't exceed the viewport height
+    bgcolor: 'background.paper',
+    border: '2px solid #006CAF',
+    borderRadius: '10px',
+    boxShadow: 24,
+    p: 4,
+    overflowY: 'auto' // Enable scrolling if content overflows
+};
+
+export default function AcceptSchoolModal() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <button
+                className="rounded-xl w-[40px] h-[40px] text-orange-600 hover:bg-orange-100"
+                onClick={handleOpen}>
+                <FontAwesomeIcon icon={faEye}/>
+            </button>
+            <Modal open={open} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                <Box sx={style}>
+                    <button
+                        className="absolute top-[15px] right-[15px] text-red-500 hover:text-gray-700 p-2 w-[40px]
+                        h-[40px] bg-white shadow-lg rounded-lg flex justify-center items-center"
+                        onClick={handleClose}
+                    >
+                        <FontAwesomeIcon icon={faTimes} size="lg"/>
+                    </button>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        School Registration Request
+                    </Typography>
+                    <section
+                        className='bg-white flex flex-row flex-wrap items-center justify-center mt-5 p-5 rounded-xl shadow-md'>
+                        <section
+                            className='w-full flex flex-row flex-wrap items-center justify-center'>
+                            <div className="w-full items-start flex my-2">
+                                <h3 className="font-medium">View School Details</h3>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <TextField
+                                    name="schoolName"
+                                    placeholder={'School name'}
+                                    important={"*"}
+                                    label={'School name'}
+                                    type="password"
+                                    disabled={true}
+
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select province"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select district"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select zonal"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Level of School"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Type of School"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Grade Span"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select gender"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select ethnicity"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select language medium"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select student Population"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <DropdownField
+                                    important={"*"}
+                                    label={"Select teacher Population"}
+                                    mt={"12px"}
+                                    mb={"12px"}
+                                    ml={"12px"}
+                                    mr={"12px"}
+                                    options={
+                                        [
+                                            {label: 'Select...', value: ''},
+                                        ]
+                                    }
+                                    disabled={true}
+                                />
+                                <TextField
+                                    name="classCount"
+                                    placeholder={'Class count'}
+                                    important={"*"}
+                                    label={'Number of Grade 1 classes'}
+                                    type="password"
+                                    disabled={true}
+
+                                />
+                            </div>
+                        </section>
+                        <section
+                            className='w-full flex flex-row flex-wrap items-center justify-center'>
+                            <div className="w-full items-start flex my-2">
+                                <h3 className="font-medium">Principal Details</h3>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <TextField
+                                    name="name"
+                                    placeholder={'ex- Nimal'}
+                                    label={'Name'}
+                                    important={"*"}
+                                    disabled={true}
+                                />
+                                <TextField
+                                    name="contact"
+                                    placeholder={'ex- 070 000 0000'}
+                                    label={'Contact'}
+                                    important={"*"}
+                                    disabled={true}
+                                />
+                                <TextField
+                                    name="nic"
+                                    placeholder={'ex- 000000000000 or 000000000v'}
+                                    label={'NIC'}
+                                    important={"*"}
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <TextField
+                                    name="username"
+                                    placeholder={'ex- Isuru123'}
+                                    label={'Username'}
+                                    important={"*"}
+                                    disabled={true}
+                                />
+                                <TextField
+                                    name="email"
+                                    placeholder={'ex- example@gmail.com'}
+                                    label={'Email'}
+                                    important={"*"}
+                                    disabled={true}
+                                />
+                                <div className='grow w-[220px] mx-3 my-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>Password</label>
+                                    </div>
+                                    <input
+                                        className={`text-input p-[7px]`}
+                                        type={"text"}
+                                        placeholder={"*******"}
+                                        disabled={true}
+                                        name={"Password"}
+                                    ></input>
+                                    <div className={`h-[5px]`}>
+                                        <small
+                                            className={`text-start text-red-600 block`}>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <TextArea
+                                    name="adminAddress"
+                                    placeholder={'ex- ABC Road, Galle'}
+                                    label={'Address'}
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <div className='grow mx-3 mt-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>NIC front side</label>
+                                    </div>
+                                </div>
+                                <div className="w-full h-[400px] bg-blue-950 rounded-md mx-3 my-3"></div>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <div className='grow mx-3 mt-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>NIC back side</label>
+                                    </div>
+                                </div>
+                                <div className="w-full h-[400px] bg-blue-950 rounded-md mx-3 my-3"></div>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <div className='grow mx-3 mt-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>MoE ID card front side</label>
+                                    </div>
+                                </div>
+                                <div className="w-full h-[400px] bg-blue-950 rounded-md mx-3 my-3"></div>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <div className='grow mx-3 mt-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>MoE ID card back side</label>
+                                    </div>
+                                </div>
+                                <div className="w-full h-[400px] bg-blue-950 rounded-md mx-3 my-3"></div>
+                            </div>
+                            <div className='flex flex-row flex-wrap items-center justify-center w-full'>
+                                <div className='grow mx-3 mt-3 gap-1 flex flex-col justify-start'>
+                                    <div className='flex flex-row'>
+                                        <label className='text-black flex justify-start'>Appointment letter</label>
+                                    </div>
+                                </div>
+                                <div className="w-full h-[1000px] bg-blue-950 rounded-md mx-3 my-3"></div>
+                            </div>
+                        </section>
+                        <div className='flex flex-row flex-wrap items-center justify-end w-full'>
+                            <Button
+                                name={'Reject'}
+                                color={'bg-red-600'}
+                            />
+                            <Button
+                                name={'Accept'}
+                                color={'bg-blue-600'}
+                            />
+                        </div>
+                    </section>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
